0b5778cbe28cc07ed15ea1a146ec79c3575dcc3c
Eedrisserh <eedrissebraheem@gmail.com>
2022-09-22 00:39:09 -0700

    Hexadecimal

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    if num < 0:
-        num *= -1
-    rem = num % 10
+    if number < 0:
+        number *= -1
+    rem = number % 10
     print("{:d}".format(rem), end="")
     return rem
