d018ed0c7eca67552b0e0ad7cc5896530a0199f8
Eedrisserh <eedrissebraheem@gmail.com>
2022-09-29 03:35:15 -0700

    fixing 3-infinite add

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    """Print the addition of all arguments."""
+    """Program to infinitely add values"""
     import sys
 
-    total = 0
-    for i in range(len(sys.argv) - 1):
-        total += int(sys.argv[i + 1])
-    print("{}".format(total))
+    n = len(sys.argv)
+    result = 0
+    for i in range(1, n):
+        result += int(sys.argv[i])
+    print("{}".format(result))
